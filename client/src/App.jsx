e7349d24c5646cd7980526b63be3338bc60a254b
Nilay Sharma <nilaysharma642@gmail.com>
2023-08-05 11:30:00 +0530

    fix: tailwind not workign

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <main className="main">App</main>;
+import {Route, Routes} from 'react-router-dom';
+
+export default function App() {
+  return (
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">Sidebar</div>
+      <div className='="flex-1 max-sm:w-full max-w[1290px] mx-auto sm:pr-5'>
+        Navbar
+      </div>
+    </div>
+  );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App';
 import {ThirdwebProvider} from '@thirdweb-dev/react';
 import {BrowserRouter as Router} from 'react-router-dom';
+import './index.css';
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
