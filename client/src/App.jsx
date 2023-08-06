078c617eeb989854d76b0e97b808f9116d0e675e
Nilay Sharma <nilaysharma642@gmail.com>
2023-08-06 11:30:00 +0530

    feat: creating side and navbar components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import {Route, Routes} from 'react-router-dom';
 import {CampaignDetails, CreateCampaign, Home, Profile} from './pages';
+import {Navbar, Sidebar} from './components';
 
 export default function App() {
   return (
     <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
-      <div className="sm:flex hidden mr-10 relative">Sidebar</div>
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
       <div className='="flex-1 max-sm:w-full max-w[1290px] mx-auto sm:pr-5'>
-        Navbar
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
